7821c18ec7dbca528d33a7a212798368adb4fe21
ThanhTung <ismethanhtung@gmail.com>
2026-01-14 01:41:19 +0700

    change name

--- bootstrap/terraform-backend/terraform.tfvars
+++ bootstrap/terraform-backend/terraform.tfvars
@@ -1,7 +1,7 @@
 # Terraform backend configuration
 environment      = "global"
-project_name     = "project"
-state_bucket_name = "project-infra-terraform-state"
-lock_table_name   = "project-infra-terraform-lock"
+project_name     = "polygo"
+state_bucket_name = "polygo-infra-terraform-state"
+lock_table_name   = "polygo-infra-terraform-lock"
 billing_mode      = "PAY_PER_REQUEST"
 hash_key          = "LockID"
--- bootstrap/terraform-backend/variables.tf
+++ bootstrap/terraform-backend/variables.tf
@@ -13,13 +13,13 @@ variable "project_name" {
 variable "state_bucket_name" {
   description = "Name of the S3 bucket for Terraform state"
   type        = string
-  default     = "polygo-infra-terraform-state"
+  default     = "project-infra-terraform-state"
 }
 
 variable "lock_table_name" {
   description = "Name of the DynamoDB table for Terraform state locking"
   type        = string
-  default     = "polygo-infra-terraform-lock"
+  default     = "project-infra-terraform-lock"
 }
 
 variable "billing_mode" {
